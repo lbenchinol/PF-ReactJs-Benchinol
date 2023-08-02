d53bf744d374a85b1ee4a0a0049400abf43d535a
Lucas Benchinol <lucasbenchinol@gmail.com>
2023-08-02 18:11:36 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 import { Box } from '@chakra-ui/react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+/* import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
-import { About } from './components/About'
+import { About } from './components/About' */
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-
+    <>
       <Box bg='#4193bf'>
         <NavBar />
       </Box>
-
-      <Routes>
-
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/about' element={<About />} />
-
-
-        <ItemListContainer />
-        {/* <ItemDetailContainer/> */}
-
-
-        </Routes>
-        </BrowserRouter>
-      )
-    }
+      <ItemListContainer />
+    </>
+  )
+}
 
 export default App
+
+
+
+
+
+{/* <BrowserRouter>
+
+<Box bg='#4193bf'>
+  <NavBar />
+</Box>
+
+<Routes>
+
+  <Route exact path='/' element={<Home />} />
+  <Route exact path='/about' element={<About />} />
+
+
+  <ItemListContainer />
+  <ItemDetailContainer/>
+
+
+  </Routes>
+  </BrowserRouter> */}
